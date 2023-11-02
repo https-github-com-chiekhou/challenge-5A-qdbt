4f87605f3ddbe9bc680f6235cf3f41a477ae5c07
chiekhou <chiekhou.traore@gmail.com>
2023-11-02 14:19:46 +0100

    install user entity with route auth jwt and user routes

--- Back/migrations/Version20231101154453.php
+++ Back/migrations/Version20231101154453.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230913220845 extends AbstractMigration
+final class Version20231101154453 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -19,9 +19,13 @@ final class Version20230913220845 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
+        // this up() migration is auto-generated, please modify it to your needs
+
     }
 
     public function down(Schema $schema): void
     {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
     }
 }
