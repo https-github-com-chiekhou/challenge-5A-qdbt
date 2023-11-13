81d25d0f6b557570ab35c9b276386f4d7e271cc2
chiekhou <chiekhou.traore@gmail.com>
2023-11-13 11:26:26 +0100

    udpate env files

--- Front/src/main.jsx
+++ Front/src/main.jsx
@@ -1,11 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider
-} from 'react-router-dom'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom"
 // import 'bootstrap/dist/css/bootstrap.min.css'
 import './assets/styles/bootstrap.custom.css'
 import './assets/styles/index.css'
