877ccf1de423223ad6daab4adfd020a3ca7fc135
Eddy Gomet <eddygomet@gmail.com>
2024-02-10 23:55:39 +0100

    .feat ajout d'api feedabck pour les prestataires

--- Back/src/Entity/FeedBack.php
+++ Back/src/Entity/FeedBack.php
@@ -2,10 +2,44 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\Link;
+use ApiPlatform\Metadata\Post;
 use App\Repository\FeedbackRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: FeedbackRepository::class)]
+#[ApiResource(
+    security: 'is_granted("ROLE_USER")',
+    normalizationContext: ['groups' => ['feedback: read']],
+    denormalizationContext: ['groups' => ['feedback: create']],
+    operations: [
+        new Post(
+            uriTemplate: '/prestataire/{id}/comment',
+            uriVariables: [
+                'id' => new Link(fromClass: Prestataire::class, fromProperty: 'id', toProperty: 'prestataire')
+            ],
+            security: 'is_granted("ROLE_USER")',
+        ),
+        new Get(
+            uriTemplate: '/prestataire/{id}/getComments',
+            uriVariables: [
+                'id' => new Link(fromClass: Prestataire::class, fromProperty: 'id', toProperty: 'prestataire')
+            ],
+            security: 'is_granted("ROLE_USER")',
+        ),
+        new Delete(
+            uriTemplate: '/prestataire/{id}/deletetComments',
+            uriVariables: [
+                'id' => new Link(fromClass: Prestataire::class, fromProperty: 'id', toProperty: 'prestataire')
+            ],
+            security: 'is_granted("ROLE_USER")',
+        )
+    ]
+)]
 class Feedback
 {
     #[ORM\Id]
@@ -13,16 +47,20 @@ class Feedback
     #[ORM\Column]
     private ?int $id = null;
 
+    #[Groups(['feedback: create', 'feedback: read'])]
     #[ORM\Column]
     private ?int $note = null;
 
+    #[Groups(['feedback: create', 'feedback: read'])]
     #[ORM\Column(length: 255)]
     private ?string $commentaire = null;
 
+    #[Groups(['feedback: create','feedback: read'])]
     #[ORM\ManyToOne(inversedBy: 'feedback')]
     #[ORM\JoinColumn(nullable: false)]
     private ?User $client = null;
 
+    #[Groups(['feedback: create','feedback: read'])]
     #[ORM\ManyToOne(inversedBy: 'feedback')]
     #[ORM\JoinColumn(nullable: false)]
     private ?Prestataire $prestataire = null;
